32b6baeea91bd0ee1bce6fb6025708823465c96a
Sandra Coka <25-022036@riseba.lv>
2026-02-11 20:55:01 +0200

    step 3

---
+++ my_script.sql
@@ -0,0 +1,11 @@
+create database my_db;
+
+create schema my_first_schema;
+
+create table users (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
